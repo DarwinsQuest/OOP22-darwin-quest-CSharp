c4c83a16b83bd3eb8363fc0c402b44832b048cf6
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-06-23 22:51:37 +0200

    Added tests for the damage calculation

--- OOP22-darwin-quest-CSharp-Test/RaffaeleMarrazzo/TestMove.cs
+++ OOP22-darwin-quest-CSharp-Test/RaffaeleMarrazzo/TestMove.cs
@@ -1,3 +1,5 @@
+using OOP22_darwin_quest_CSharp.EnricoMarchionni.Banion;
+using System.Collections.Immutable;
 using OOP22_darwin_quest_CSharp.EnricoMarchionni.Element;
 using OOP22_darwin_quest_CSharp.RaffaeleMarrazzo.Move;
 
@@ -6,9 +8,11 @@ namespace OOP22_darwin_quest_CSharp_Test.RaffaeleMarrazzo;
 [TestFixture]
 public class TestMove
 {
+    private const uint MIN_DAMAGE_INFLICTED = 1;
     private const uint ILLEGAL_BASE_DAMAGE = 0;
     private const uint LEGAL_BASE_DAMAGE_1 = 20;
     private const uint LEGAL_BASE_DAMAGE_2 = 10;
+    private const uint HP = 100;
     private const String MOVE_NAME_1 = "Fireball";
     private const String MOVE_NAME_2 = "Splash";
     private readonly IElement neutral = new Neutral();
@@ -27,6 +31,30 @@ public class TestMove
     }
 
     [Test]
+    public void TestComputeDamage()
+    {
+        var damageMove = new BasicMove(LEGAL_BASE_DAMAGE_2, "move", new Air());
+        var banions = CreateBanions();
+        foreach (var banion in banions)
+        {
+            Assert.That(damageMove.ComputeDamage(banion), Is.GreaterThanOrEqualTo(MIN_DAMAGE_INFLICTED));
+        }
+    }
+
+    [Test]
+    public void TestPerformDamage()
+    {
+        var damageMove = new BasicMove(LEGAL_BASE_DAMAGE_2, "move", new Air());
+        var banions = CreateBanions();
+        foreach (var banion in banions)
+        {
+            var banionHpBeforeMove = banion.Hp;
+            damageMove.Perform(banion);
+            Assert.That(damageMove.ComputeDamage(banion), Is.EqualTo(banionHpBeforeMove - banion.Hp));
+        }
+    }
+
+    [Test]
     public void TestEquality()
     {
         IDamageMove m1 = new BasicMove(LEGAL_BASE_DAMAGE_1, MOVE_NAME_1, neutral);
@@ -39,6 +67,35 @@ public class TestMove
         Assert.That(m1, !Is.EqualTo(m2));
         Assert.That(m2, !Is.EqualTo(m3));
         Assert.That(m1, !Is.EqualTo(m5));
-    } 
+    }
+
+    private static IList<IBanion> CreateBanions()
+    {
+        var fire = new ImmutableElement("Fire", ImmutableHashSet.Create<string>(), ImmutableHashSet.Create<string>());
+        var water = new ImmutableElement("Water", ImmutableHashSet.Create<string>(), ImmutableHashSet.Create<string>());
+        var grass = new ImmutableElement("Grass", ImmutableHashSet.Create<string>(), ImmutableHashSet.Create<string>());
+        var air = new ImmutableElement("Air", ImmutableHashSet.Create<string>(), ImmutableHashSet.Create<string>());
+        IImmutableSet<IMove> b1Moves = ImmutableHashSet.Create<IMove>(new BasicMove(LEGAL_BASE_DAMAGE_1, "m1", fire),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m2", fire),
+                new BasicMove(LEGAL_BASE_DAMAGE_1, "m3", fire),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m4", fire));
+        IImmutableSet<IMove> b2Moves = ImmutableHashSet.Create<IMove>(new BasicMove(LEGAL_BASE_DAMAGE_1, "m1", water),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m2", water),
+                new BasicMove(LEGAL_BASE_DAMAGE_1, "m3", water),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m4", water));
+        IImmutableSet<IMove> b3Moves = ImmutableHashSet.Create<IMove>(new BasicMove(LEGAL_BASE_DAMAGE_1, "m1", grass),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m2", grass),
+                new BasicMove(LEGAL_BASE_DAMAGE_1, "m3", grass),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m4", grass));
+        IImmutableSet<IMove> b4Moves = ImmutableHashSet.Create<IMove>(new BasicMove(LEGAL_BASE_DAMAGE_1, "m1", air),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m2", air),
+                new BasicMove(LEGAL_BASE_DAMAGE_1, "m3", air),
+                new BasicMove(LEGAL_BASE_DAMAGE_2, "m4", air));
+        IBanion b1 = new Banion(fire, "b1", HP, new HashSet<IMove>(b1Moves));
+        IBanion b2 = new Banion(water, "b2", HP, new HashSet<IMove>(b2Moves));
+        IBanion b3 = new Banion(grass, "b3", HP, new HashSet<IMove>(b3Moves));
+        IBanion b4 = new Banion(air, "b4", HP, new HashSet<IMove>(b4Moves));
+        return new List<IBanion> { b1, b2, b3, b4 };
+    }
 
 }
